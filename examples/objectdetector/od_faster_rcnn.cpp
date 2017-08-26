f70002d5bebd1f0d37bd6d7d9da8ca6b0f1bc7a2
Gautam Malu <gautam.maloo@gmail.com>
2017-08-26 18:52:14 +0530

    Added usage example in faster rcnn examples

--- examples/objectdetector/od_faster_rcnn.cpp
+++ examples/objectdetector/od_faster_rcnn.cpp
@@ -8,17 +8,22 @@ int main(int argc, char **argv){
 	od::g2d::ODConvDetector *faster_rcnn = new od::g2d::ODConvDetector();
 	string conf = "examples/objectdetector/faster_rcnn_models/config/voc_config.json";
 	faster_rcnn->setConfig(conf);
-	string model_def(argv[1]);
-	string model_weight(argv[2]);
-	faster_rcnn->initDetector(model_def, model_weight);
-	string img_src(argv[3]);
-	od::ODSceneImage *img = new od::ODSceneImage(img_src);
-	od::ODDetections2D *detections = faster_rcnn->detectOmni(img);
-	std::cout << "Number of detections: "<< detections->size() << std::endl;
-	
-	cv::namedWindow( "Display window",cv::WINDOW_NORMAL ); // Create a window for display.
-    	cv::imshow( "Display window",  detections->renderMetainfo(*img).getCVImage());// Show our image inside it.
-    	cv::waitKey(0); // Wait for a keystroke in the window
-	
+	if (argc < 4){
+		std::cout << "usage: od_faster_rcnn_cam model_definition model_weights image_source" << std::endl;
+		return 0;
+	}
+	else{
+		string model_def(argv[1]);
+		string model_weight(argv[2]);
+		faster_rcnn->initDetector(model_def, model_weight);
+		string img_src(argv[3]);
+		od::ODSceneImage *img = new od::ODSceneImage(img_src);
+		od::ODDetections2D *detections = faster_rcnn->detectOmni(img);
+		std::cout << "Number of detections: "<< detections->size() << std::endl;
+
+		cv::namedWindow( "Display window",cv::WINDOW_NORMAL ); // Create a window for display.
+		cv::imshow( "Display window",  detections->renderMetainfo(*img).getCVImage());// Show our image inside it.
+		cv::waitKey(0); // Wait for a keystroke in the window
+	}
 	return 0;
 }
--- examples/objectdetector/od_faster_rcnn_cam.cpp
+++ examples/objectdetector/od_faster_rcnn_cam.cpp
@@ -9,23 +9,30 @@ int main(int argc, char **argv){
 	od::g2d::ODConvDetector *faster_rcnn = new od::g2d::ODConvDetector();
 	string conf = "examples/objectdetector/faster_rcnn_models/config/voc_config.json";
 	faster_rcnn->setConfig(conf);
-	string model_def(argv[1]); 
-	string model_weight(argv[2]);
-	faster_rcnn->initDetector(model_def, model_weight);
 
-	//get scenes
-	od::ODFrameGenerator<od::ODSceneImage, od::GENERATOR_TYPE_DEVICE> frameGenerator(0);
-	//GUI
-	cv::namedWindow("Overlay", cv::WINDOW_NORMAL);
-	while(frameGenerator.isValid() && cv::waitKey(30) != 27)
-	{
-		od::ODSceneImage * scene = frameGenerator.getNextFrame();
-		//Detect
-		od::ODDetections2D *detections =  faster_rcnn->detectOmni(scene);
-		if(detections->size() > 0)
-			cv::imshow("Overlay", detections->renderMetainfo(*scene).getCVImage());
-		else
-			cv::imshow("Overlay", scene->getCVImage());
+	if (argc < 3){
+		std::cout << "usage: od_faster_rcnn_cam model_definition model_weights" << std::endl;
+		return 0;
+	}
+	else{
+		string model_def(argv[1]); 
+		string model_weight(argv[2]);
+		faster_rcnn->initDetector(model_def, model_weight);
+
+		//get scenes
+		od::ODFrameGenerator<od::ODSceneImage, od::GENERATOR_TYPE_DEVICE> frameGenerator(0);
+		//GUI
+		cv::namedWindow("Overlay", cv::WINDOW_NORMAL);
+		while(frameGenerator.isValid() && cv::waitKey(30) != 27)
+		{
+			od::ODSceneImage * scene = frameGenerator.getNextFrame();
+			//Detect
+			od::ODDetections2D *detections =  faster_rcnn->detectOmni(scene);
+			if(detections->size() > 0)
+				cv::imshow("Overlay", detections->renderMetainfo(*scene).getCVImage());
+			else
+				cv::imshow("Overlay", scene->getCVImage());
+		}
 	}
 	return 0;
 }
